0fbf9a7b7fe008e140b82e365293ddd4aff349dd
Shane Lee <me@shanel.ee>
2020-03-28 08:40:53 +0000

    Updated background and added tests

---
+++ test/backgroundSpec.js
@@ -0,0 +1,21 @@
+describe('Test taking a search query and appending banned sources to it', () => {
+	it('should add banned sources to search', () => {
+		const details = {
+			'url': 'https://www.google.com/search?q=test&oq=test&aqs=chrome.0.69i59l3j69i61j69i60l2j69i65l2.464j0j1&sourceid=chrome&ie=UTF-8'}
+		const expectation = 'https://www.google.com/search?q=test+-site:businessinsider.com+-site:dailymail.co.uk+-site:dailyexpress.co.uk+-site:fool.co.uk'
+		expect(blockSources(details).redirectUrl).toEqual(expectation)
+	})
+
+	it('should not alert search on any request after the first within 100 milliseconds', () => {
+		const details = {
+			'url': 'https://www.google.com/search?q=test&oq=test&aqs=chrome.0.69i59l3j69i61j69i60l2j69i65l2.464j0j1&sourceid=chrome&ie=UTF-8'}
+		const expectation = 'https://www.google.com/search?q=test+-site:businessinsider.com+-site:dailymail.co.uk+-site:dailyexpress.co.uk+-site:fool.co.uk'
+		expect(blockSources(details)).toEqual(expectation)
+		expect(blockSources(details)).toEqual(undefined)
+	})
+	
+	it('should not alter non-google search URL', () => {
+		const details = {'url': 'http://test.com'}
+		expect(blockSources(details)).toEqual(undefined)
+	})
+})
